a47a6e90ee62c602dea85a4a125e79019b86e912
Jorge Canut <jorgecaru3@gmail.com>
2026-01-29 12:15:36 +0100

    fuck bsm

--- Core/Src/main.cpp
+++ Core/Src/main.cpp
@@ -11,16 +11,19 @@ int main(void) {
     Hard_fault_check();
     STLIB::start("00:aa:6b:ae:19:6b", "192.168.1.7");   
     
+    HVBMS::Comms::start();
     
-
     Scheduler::register_task(1000, [](){
-        BSM.check_transitions();
+        if (HVBMS::Comms::tcp_connected()){
+            HVBMS::Actuators::operational_led();
+        }
     });
 
-    BSM.start();
+    //BSM.start();
 
     while (1) {
         STLIB::update();
+
     }
 }
 
