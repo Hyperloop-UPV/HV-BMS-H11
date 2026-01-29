f68b879517a049274f8abce0e3b06cf9ab5c97bb
Jorge Canut <jorgecaru3@gmail.com>
2026-01-29 12:57:37 +0100

    antes de main

--- Core/Src/main.cpp
+++ Core/Src/main.cpp
@@ -9,16 +9,19 @@ int main(void) {
 #endif
     
     Hard_fault_check();
+
+    HVBMS::Actuators::led_fault();
+    HVBMS::Actuators::led_operational();
+    HVBMS::Actuators::led_fault().turn_on();
+    HVBMS::Actuators::led_operational().turn_on();
+
     STLIB::start("00:aa:6b:ae:19:6b", "192.168.1.7");   
     
     //HVBMS::Comms::start();
 
 
     //BSM.start();
-    HVBMS::Actuators::led_fault();
-    HVBMS::Actuators::led_operational();
-    HVBMS::Actuators::led_fault().turn_on();
-    HVBMS::Actuators::led_operational().turn_on();
+
     
     while (1) {
         STLIB::update();
